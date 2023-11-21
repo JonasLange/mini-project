438ebb717be104993f1837ff08ab4f5ab197b402
Jonas Lange <jonas.lange@hotmail.de>
2023-11-21 14:59:20 +0100

    split environment from learner

--- highway_env.py
+++ highway_env.py
@@ -23,7 +23,6 @@ class Highway(ParallelEnv):
         if not self.traci_connected:
             print("first start")
             self._start()
-            #traci.close()
             self.traci_connected=True
         else:
             print("resetting existing simulation")
@@ -42,6 +41,7 @@ class Highway(ParallelEnv):
             observations[agent]=int(speed)
         infos = {a: {} for a in self.agents}
         return observations, infos
+    
     def step(self, actions):
         #send the actions
         print(actions)
@@ -69,6 +69,7 @@ class Highway(ParallelEnv):
             truncated[agent]=False
             infos[agent]=None
         return observations,rewards,terminated,truncated,infos
+    
     def render(self):
         pass
     
@@ -83,7 +84,6 @@ class Highway(ParallelEnv):
     
     @functools.lru_cache(maxsize=None)
     def action_space(self, agent):
-        #return spaces.Discrete(3)
         return spaces.Discrete(120, start=10)
 
     def _start(self):
@@ -103,86 +103,17 @@ class Highway(ParallelEnv):
         Adds the learner to the simulation
         """
         topology = {}
-
-        # add a vehicle that wants to join the platoon
         vid = "learner"
         add_platooning_vehicle(self.plexe, vid, 10, 1, 10, 5, real_engine)
         self.plexe.set_fixed_lane(vid, 1, safe=False)
         traci.vehicle.setSpeedMode(vid, 0)
         self.plexe.set_active_controller(vid, ACC)
-        #plexe.set_path_cacc_parameters(vid, distance=JOIN_DISTANCE)
         return topology
     def _reward(self, emmisions, speed):
         return speed/emmisions
 
-import supersuit as ss
-from stable_baselines3 import PPO
-from stable_baselines3.ppo import MlpPolicy
-import time
-def train(steps = 10_000):
-    env = Highway()
-    env.reset()
-    print(f"Starting training on {str(env.metadata['name'])}.")
-    env = ss.pettingzoo_env_to_vec_env_v1(env)
-    env = ss.concat_vec_envs_v1(env, 8, num_cpus=1, base_class="stable_baselines3")
 
-    model = PPO(MlpPolicy,env,verbose=3,batch_size=256,)
-    model.learn(total_timesteps=steps)
-    model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
-
-    print("Model has been saved.")
-    print(f"Finished training on {str(env.unwrapped.metadata['name'])}.")
-
-    env.close()
-
-from ray import tune
-from ray.rllib.algorithms.ppo import PPOConfig
-def train_ray():
-    print("now training")
-    config = (
-        PPOConfig()
-        .environment(env="highway", clip_actions=True)
-        .rollouts(num_rollout_workers=4, rollout_fragment_length=128)
-        .training(
-            train_batch_size=512,
-            lr=2e-5,
-            gamma=0.99,
-            lambda_=0.9,
-            use_gae=True,
-            clip_param=0.4,
-            grad_clip=None,
-            entropy_coeff=0.1,
-            vf_loss_coeff=0.25,
-            sgd_minibatch_size=64,
-            num_sgd_iter=10,
-        )
-        .debugging(log_level="ERROR")
-        .framework(framework="torch")
-        .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
-    )
-
-    tune.run(
-        "PPO",
-        name="PPO",
-        stop={"timesteps_total": 5000000},
-        checkpoint_freq=10,
-        local_dir="ray_results/" + "highway",
-        config=config.to_dict(),
-    )
-
-
-def env_creator():
-    env = Highway()
-    env = ss.dtype_v0(env,int)
-    return env
-
-#For Testing
-import ray
-from ray.tune.registry import register_env
 from pettingzoo.test import parallel_api_test
 if __name__ == "__main__":
-    ray.init()
-    register_env("highway",env_creator)
-    train_ray()
-    #env = Highway()
-    #parallel_api_test(env, num_cycles=500)#testing for 5 seconds each
+    env = Highway()
+    parallel_api_test(env, num_cycles=500)#testing for 5 seconds each
